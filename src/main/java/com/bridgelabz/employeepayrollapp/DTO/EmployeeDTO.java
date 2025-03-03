7b01ce37621cb2b70c334149c22e66bd7f559315
Tarun Dubey <tarun2019dubey@gmail.com>
2025-03-03 13:23:53 +0530

    [Tarun] Add. validate name

--- src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/DTO/EmployeeDTO.java
@@ -1,16 +1,25 @@
 package com.bridgelabz.employeepayrollapp.DTO;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Pattern;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDTO {
+    @NotBlank(message = "Name cannot be empty")
+    @Pattern(
+            regexp = "^[A-Z][a-zA-Z\\s]*$",
+            message = "Name must start with a capital letter and contain only alphabets"
+    )
     private String name;
     private int salary;
+    @NotBlank(message = "department cannot be empty")
     private String department;
 
 
